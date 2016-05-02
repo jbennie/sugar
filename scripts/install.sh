99d0c77428f4aa556c3fc8a4ea7b0198a7e96126
Olivier Bossel <olivier.bossel@esl-education.org>
2016-05-02 17:02:16 +0200

    up

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,14 +5,14 @@ mv src/js/sugar/ js/
 mv src/sass/sugar/ sass/
 
 # clean repo
-# rm -rf src
-# rm -rf *.html
-# rm -rf pages
-# rm -rf assets
-# rm -rf config.rb
-# rm -rf favicon.ico
-# rm -rf .DS_Store
-# rm -rf *.coffee
-# # rm -rf node_modules
-# rm -rf scripts
-# rm -rf bower.json
+rm -rf src
+rm -rf *.html
+rm -rf pages
+rm -rf assets
+rm -rf config.rb
+rm -rf favicon.ico
+rm -rf .DS_Store
+rm -rf *.coffee
+rm -rf node_modules
+rm -rf scripts
+rm -rf bower.json
