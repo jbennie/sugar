bd2c03f8e8451c43cb47fc2d487ce8651f468dbd
Olivier Bossel <olivier.bossel@esl-education.org>
2016-07-04 11:36:12 +0200

    add the imageLoaded dom function

---
+++ src/js/sugar/dom/imageLoaded.js
@@ -0,0 +1,31 @@
+/**
+ * Detect when an image is loaded
+ */
+import Pro from 'promise-polyfill'
+if ( ! window.Promise) {
+	window.Promise = Pro;
+}
+export default function imageLoaded(img, callback = null) {
+	return new Promise((resolve, reject) => {
+		// check if image is already loaded
+		if (img.complete) {
+			// resolve promise
+			resolve(img);
+			// call the callback if exist
+			callback != null && callback(img);
+		} else {
+			// wait until loaded
+			img.addEventListener('load', (e) => {
+				// resolve the promise
+				resolve(img);
+				// callback if exist
+				callback != null && callback(img);
+			});
+			// listen for error
+			img.addEventListener('error', (e) => {
+				// reject
+				reject(e);
+			});
+		}
+	});
+}
