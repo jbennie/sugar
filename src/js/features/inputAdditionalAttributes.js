2b588afca399e8815bfcb77393fce910e8379d53
Olivier Bossel <olivier.bossel@esl-education.org>
2016-11-22 15:26:17 +0100

    update input features

--- src/js/features/inputAdditionalAttributes.js
+++ src/js/features/inputAdditionalAttributes.js
@@ -1,32 +1,47 @@
 import sTemplateIntegrator from '../core/sTemplateIntegrator'
 import fastdom from 'fastdom'
 
-function handleInputAttributes(e) {
-	if ( ! e.target || ! e.target.tagName) return;
-	switch(e.target.tagName) {
+function handleInputAttributes(eOrElm) {
+	const field = eOrElm.target ? eOrElm.target : eOrElm;
+	if ( ! field || ! field.tagName) return;
+	switch(field.tagName) {
 		case 'INPUT':
 		case 'TEXTAREA':
 		case 'SELECT':
 			fastdom.mutate(() => {
-				if (e.type && (e.type === 'checkbox' || e.type === 'radio')) return;
-				if (e.target.value && ! e.target.hasAttribute('has-value')) {
-					e.target.setAttribute('has-value', true);
-					e.target.removeAttribute('empty');
-				} else if ( ! e.target.value && e.target.hasAttribute('has-value')) {
-					e.target.removeAttribute('has-value');
-					if ( ! e.target.hasAttribute('empty')) {
-						e.target.setAttribute('empty', true);
+				if (field.type && (field.type === 'checkbox' || field.type === 'radio')) return;
+				if (field.value && ! field.hasAttribute('has-value')) {
+					field.setAttribute('has-value', true);
+					field.removeAttribute('empty');
+				} else if ( ! field.value && field.hasAttribute('has-value')) {
+					field.removeAttribute('has-value');
+					if ( ! field.hasAttribute('empty')) {
+						field.setAttribute('empty', true);
 					}
 				}
-				if (e.target.value && ! e.target.hasAttribute('dirty')) {
-					e.target.setAttribute('dirty', true);
+				if (field.value && ! field.hasAttribute('dirty')) {
+					field.setAttribute('dirty', true);
 				}
 			});
 		break;
 	}
 }
+
+function handleFormReset(e) {
+	// loop on each form elements
+	[].forEach.call(e.target.elements, (field) => {
+		// reset the field attributes
+		handleInputAttributes(field);
+		// remove dirty attribute
+		fastdom.mutate(() => {
+			field.removeAttribute('dirty');
+		});
+	});
+}
+
 document.addEventListener('change', handleInputAttributes);
 document.addEventListener('keyup', handleInputAttributes);
+document.addEventListener('reset', handleFormReset);
 
 sTemplateIntegrator.registerComponentIntegration([HTMLInputElement,HTMLSelectElement], (input) => {
 	sTemplateIntegrator.ignore(input, {
