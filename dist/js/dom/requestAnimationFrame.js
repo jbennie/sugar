612627095182886f8041e07c1fb90eb773328740
Olivier Bossel <olivier.bossel@gmail.com>
2016-12-21 17:54:23 +0100

    Fix issue with module-scale that has been removed but not totally

--- dist/js/dom/requestAnimationFrame.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-/**
- * Proxy for the window.requestAnimationFrame function
- */
-exports.default = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
