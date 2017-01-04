befa074e3679df9bab18eefcbf4dc4ba77544732
Olivier Bossel <olivier.bossel@gmail.com>
2017-01-04 16:39:32 +0100

    Try using in-view as in viewport detector method

--- dist/js/dom/isInViewport.js
+++ dist/js/dom/isInViewport.js
@@ -7,29 +7,42 @@ var _getBoundingClientRect = require('./getBoundingClientRect');
 
 var _getBoundingClientRect2 = _interopRequireDefault(_getBoundingClientRect);
 
+var _inView = require('in-view');
+
+var _inView2 = _interopRequireDefault(_inView);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/**
+ * Check if the passed HTMLElement is in the viewport or not
+ *
+ * @name 		isInViewport
+ * @param 		{HTMLElement} 				elm  		The element to insert
+ * @param 		{Object} 					offset 		An object of top, right, bottom and left offset used to detect the status
+ * @return 		{Boolean								If the element is in the viewport or not
+ *
+ * @example  	js
+ * import isInViewport from 'sugarcss/js/dom/isInViewport'
+ * if (isInViewport(myCoolHTMLElement) {
+ * 		// i'm in the viewport
+ * }
+ *
+ * @author 		Olivier Bossel <olivier.bossel@gmail.com>
+ */
 function isInViewport(elm) {
   var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : { top: 0, right: 0, bottom: 0, left: 0 };
 
 
-  var rect = (0, _getBoundingClientRect2.default)(elm);
-  var wh = window.innerHeight || document.documentElement.clientHeight;
-  var ww = window.innerWidth || document.documentElement.clientWidth;
-  return rect.top - wh - offset.top <= 0 && rect.bottom + offset.bottom >= 0 && rect.left - ww - offset.left <= 0 && rect.right + offset.right >= 0;
-} /**
-   * Check if the passed HTMLElement is in the viewport or not
-   *
-   * @name 		isInViewport
-   * @param 		{HTMLElement} 				elm  		The element to insert
-   * @param 		{Object} 					offset 		An object of top, right, bottom and left offset used to detect the status
-   * @return 		{Boolean								If the element is in the viewport or not
-   *
-   * @example  	js
-   * import isInViewport from 'sugarcss/js/dom/isInViewport'
-   * if (isInViewport(myCoolHTMLElement) {
-   * 		// i'm in the viewport
-   * }
-   *
-   * @author 		Olivier Bossel <olivier.bossel@gmail.com>
-   */
+  inView.offset(offset);
+  return inView.is(elm);
+
+  // const rect = __getBoundingClientRect(elm);
+  // const wh = (window.innerHeight || document.documentElement.clientHeight);
+  // const ww = (window.innerWidth || document.documentElement.clientWidth);
+  // return (
+  // 	rect.top - wh - offset.top <= 0
+  // 	&& rect.bottom + offset.bottom  >= 0
+  // 	&& rect.left - ww - offset.left <= 0
+  // 	&& rect.right + offset.right >= 0
+  // );
+}
--- src/js/dom/isInViewport.js
+++ src/js/dom/isInViewport.js
@@ -15,15 +15,19 @@
  * @author 		Olivier Bossel <olivier.bossel@gmail.com>
  */
 import __getBoundingClientRect from './getBoundingClientRect'
+import __inView from 'in-view'
 export default function isInViewport(elm, offset = { top:0, right:0, bottom:0, left:0 }) {
 
-	const rect = __getBoundingClientRect(elm);
-	const wh = (window.innerHeight || document.documentElement.clientHeight);
-	const ww = (window.innerWidth || document.documentElement.clientWidth);
-	return (
-		rect.top - wh - offset.top <= 0
-		&& rect.bottom + offset.bottom  >= 0
-		&& rect.left - ww - offset.left <= 0
-		&& rect.right + offset.right >= 0
-	);
+	inView.offset(offset);
+	return inView.is(elm);
+
+	// const rect = __getBoundingClientRect(elm);
+	// const wh = (window.innerHeight || document.documentElement.clientHeight);
+	// const ww = (window.innerWidth || document.documentElement.clientWidth);
+	// return (
+	// 	rect.top - wh - offset.top <= 0
+	// 	&& rect.bottom + offset.bottom  >= 0
+	// 	&& rect.left - ww - offset.left <= 0
+	// 	&& rect.right + offset.right >= 0
+	// );
 }
