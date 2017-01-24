03dd8abd414fb4a18454b931d7debfaccc9364f5
Olivier Bossel <olivier.bossel@gmail.com>
2017-01-24 18:20:18 +0100

    clean repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,21 +11,13 @@ then
 	mv dist/js/ js/
 	mv src/sass/ sass/
 	mv dist/components/ components/
-	mv dist/webcomponents/ webcomponents/
 
 	# clean repo
 	rm -rf src
-	rm -rf *.html
-	rm -rf *.php
-	rm -rf pages
 	rm -rf .sass-cache
-	rm -rf assets
-	rm -rf config.rb
-	rm -rf favicon.ico
 	rm -rf .DS_Store
 	rm -rf *.coffee
 	rm -rf scripts
-	rm -rf bower.json
 
 	# update _index.scss file import reference
 	echo '@import "sass/sugar";' > _index.scss
