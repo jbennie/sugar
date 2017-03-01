42db1e6e63b02433c26707f80867ce0caedf4e36
Olivier Bossel <olivier.bossel@gmail.com>
2017-03-01 20:39:34 +0100

    clean

--- _toMigrate/webcomponents/SSelectComponent/index.js
+++
@@ -1,2 +0,0 @@
-import SSelectComponent from './js/SSelectComponent';
-export default SSelectComponent.define('s-select', SSelectComponent, 'select');
---
+++ doc/webcomponent.md
@@ -0,0 +1,3 @@
+# Webcomponent
+
+coming soon...
