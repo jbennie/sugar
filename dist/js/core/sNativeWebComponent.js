c5c8087dc43946e6a70cccf5a6bc5781b52b5362
Olivier Bossel <olivier.bossel@gmail.com>
2017-04-18 14:20:21 +0200

    fix native extend

--- dist/js/core/sNativeWebComponent.js
+++ dist/js/core/sNativeWebComponent.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+   value: true
 });
 exports.default = sNativeWebComponent;
 
@@ -14,10 +14,10 @@ var _SWebComponentMixin2 = _interopRequireDefault(_SWebComponentMixin);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function sNativeWebComponent(HTMLElementToExtend) {
-	if (typeof HTMLElementToExtend !== 'function') {
-		var _HTMLElementToExtend = function _HTMLElementToExtend() {};
-		_HTMLElementToExtend.prototype = HTMLElementToExtend.prototype;
-		HTMLElementToExtend = _HTMLElementToExtend;
-	}
-	return (0, _mixwith.mix)(HTMLElementToExtend).with(_SWebComponentMixin2.default);
+   // if (typeof HTMLElementToExtend !== 'function'){
+   var _HTMLElementToExtend = function _HTMLElementToExtend() {};
+   _HTMLElementToExtend.prototype = HTMLElementToExtend.prototype;
+   HTMLElementToExtend = _HTMLElementToExtend;
+   // }
+   return (0, _mixwith.mix)(HTMLElementToExtend).with(_SWebComponentMixin2.default);
 }
