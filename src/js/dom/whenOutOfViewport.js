8092d0f578ab38449523b0652c0c28e3948824c9
Olivier Bossel <olivier.bossel@gmail.com>
2017-09-27 17:15:55 +0200

    Revert "add intersectionobserver capability when available for the whenOutOfViewport function"
    This reverts commit a5072de530366e330fbeff2f5cabe9dbb3a02e18.

--- src/js/dom/whenOutOfViewport.js
+++ src/js/dom/whenOutOfViewport.js
@@ -7,7 +7,7 @@ import __closest from './closest'
  *
  * @name 		whenOutOfViewport
  * @param 		{HTMLElement} 				elm 				The element to monitor
- * @param 		{Number} 					[offset=50] 		An offset that represent the distance before entering the viewport for the detection
+ * @param 		{Number} 					[offset=50] 		An offset that represent the distance before entering the viewport for the detection or an object with top, right, bottom and left offsets
  * @return 		(Promise) 										The promise that will be resolved when the element exit the viewport
  *
  * @example 	js
@@ -20,69 +20,37 @@ import __closest from './closest'
  */
 export default function whenOutOfViewport(elm, offset = 50) {
 	return new Promise((resolve, reject) => {
-
-		if (window.IntersectionObserver) {
-
-			let isInViewport = false,
-				_cb = () => {
-					if ( ! isInViewport) {
-						observer.disconnect();
-						resolve(elm);
-					}
-				}
-
-			const observer = new IntersectionObserver((entries, observer) => {
-				if ( ! entries.length) return
-				const entry = entries[0]
-				if (entry.intersectionRatio > 0) {
-					isInViewport = true
-				} else {
-					isInViewport = false
-				}
-				_cb()
-			}, {
-				root: null, // viewport
-				rootMargin: `${offset}px`,
-				threshold: [0]
-			});
-
-			observer.observe(elm);
-
-		} else {
-
-			// try to get the closest element that has an overflow
-			let scrollContainerElm = document;
-			if ( ! elm._inViewportContainer) {
-				const overflowContainer = __closest(elm, '[data-in-viewport-container]');
-				if (overflowContainer) {
-					scrollContainerElm = overflowContainer;
-					elm._inViewportContainer = overflowContainer;
-				}
-			} else {
-				scrollContainerElm = elm._inViewportContainer;
+		// try to get the closest element that has an overflow
+		let scrollContainerElm = document;
+		if ( ! elm._inViewportContainer) {
+			const overflowContainer = __closest(elm, '[data-in-viewport-container]');
+			if (overflowContainer) {
+				scrollContainerElm = overflowContainer;
+				elm._inViewportContainer = overflowContainer;
 			}
-
-			let isInViewport = true,
-				_cb = () => {
-					if ( ! isInViewport) {
-						scrollContainerElm.removeEventListener('scroll', checkViewport);
-						window.removeEventListener('resize', checkViewport);
-						resolve(elm);
-					}
-				}
-			let checkViewport = __throttle((e) => {
-				isInViewport = __isInViewport(elm, offset);
-				_cb();
-			},100);
-
-			// listen for resize
-			scrollContainerElm.addEventListener('scroll', checkViewport);
-			window.addEventListener('resize', checkViewport);
-			setTimeout(() => {
-				checkViewport(null);
-			});
-
+		} else {
+			scrollContainerElm = elm._inViewportContainer;
 		}
 
+		let isInViewport = true,
+			_cb = () => {
+				if ( ! isInViewport) {
+					scrollContainerElm.removeEventListener('scroll', checkViewport);
+					window.removeEventListener('resize', checkViewport);
+					if (cb)	cb(elm);
+					resolve(elm);
+				}
+			}
+		let checkViewport = __throttle((e) => {
+			isInViewport = __isInViewport(elm, offset);
+			_cb();
+		},100);
+
+		// listen for resize
+		scrollContainerElm.addEventListener('scroll', checkViewport);
+		window.addEventListener('resize', checkViewport);
+		setTimeout(() => {
+			checkViewport(null);
+		});
 	});
 }
