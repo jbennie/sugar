f21752739a936a0d9bedf854bc0413ca012a224b
Olivier Bossel <olivier.bossel@gmail.com>
2018-05-22 21:32:24 +0200

    update the install script

---
+++ scripts/install.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const fse = require('fs-extra');
+
+// move sources
+fse.removeSync('js');
+fse.moveSync('dist/js','js');
+fse.removeSync('sass');
+fse.moveSync('src/sass','sass');
+
+// clean repo
+fse.removeSync('_toMigrate');
+fse.removeSync('dist');
+fse.removeSync('scripts');
+fse.removeSync('src');
+fse.removeSync('tests');
+fse.removeSync('.sass-cache');
+fse.removeSync('.babelrc');
+fse.removeSync('.changelogrc');
+fse.removeSync('.DS_Store');
+fse.removeSync('.editorconfig');
+fse.removeSync('.node-version');
+fse.removeSync('.travis.yml');
+fse.removeSync('favicon.ico');
+fse.removeSync('karma.conf.js');
+fse.removeSync('webpack.config.js');
+
+// update sass files at root
+fs.writeFileSync('_index.scss', '@import "sass/sugar";');
