cbebf54523f4b3a15db94ba86202691e057d0ce3
Olivier Bossel <olivier.bossel@gmail.com>
2018-05-23 11:45:49 +0200

    fix: fix small issue in the imagesLoadedAttribute feature

--- src/js/features/imagesLoadedAttribute.js
+++ src/js/features/imagesLoadedAttribute.js
@@ -1,15 +1,13 @@
 import __imageLoaded from '../dom/imageLoaded'
-document.addEventListener('DOMContentLoaded', () => {
-	document.body.addEventListener('load', (e) => {
-		if ( ! e.target.tagName) return;
-		if (e.target.tagName.toLowerCase() !== 'img') return;
-		if (e.target.hasAttribute('loaded')) return;
-		e.target.setAttribute('loaded', true);
-	}, true);
-	[].forEach.call(document.querySelectorAll('img'), (img) => {
-		__imageLoaded(img).then((img) => {
-			if (img.hasAttribute('loaded')) return;
-			img.setAttribute('loaded', true);
-		});
+document.addEventListener('load', (e) => {
+	if ( ! e.target.tagName) return;
+	if (e.target.tagName.toLowerCase() !== 'img') return;
+	if (e.target.hasAttribute('loaded')) return;
+	e.target.setAttribute('loaded', true);
+}, true);
+[].forEach.call(document.querySelectorAll('img'), (img) => {
+	__imageLoaded(img).then((img) => {
+		if (img.hasAttribute('loaded')) return;
+		img.setAttribute('loaded', true);
 	});
 });
