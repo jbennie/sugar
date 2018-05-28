18c39dacdb0d202a00d8dda9b4b2a73e08a835a3
Olivier Bossel <olivier.bossel@gmail.com>
2018-05-28 17:08:25 +0200

    add alias to coffeekraken-sugar in the karma webpack config

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,44 @@
+const path = require('path')
+
 module.exports = (config) => {
-    config.set({
-      files: [
-        'tests/bootstrap.js',
-        {
-          pattern: 'tests/**/*.tests.js',
-          watched: false
-        },
-        'tests/run.js',
-        {pattern: 'tests/img/**/*', watched: false, included: false, served: true},
-      ],
-      browsers: ['PhantomJS'],
-      frameworks: ['mocha'],
-      preprocessors: {
-        'tests/**/*.js': [ 'webpack' ]
+  config.set({
+    files: [
+      'tests/bootstrap.js',
+      {
+        pattern: 'tests/**/*.tests.js',
+        watched: false
       },
-      webpack: {
-        module: {
-          rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader'
-          }]
+      'tests/run.js',
+      {pattern: 'tests/img/**/*', watched: false, included: false, served: true}
+    ],
+    browsers: ['PhantomJS'],
+    frameworks: ['mocha'],
+    preprocessors: {
+      'tests/**/*.js': [ 'webpack' ]
+    },
+    webpack: {
+      resolve: {
+        alias: {
+          'coffeekraken-sugar': path.resolve(__dirname + '/dist/')
         }
       },
-      webpackMiddleware: {
-        stats: 'errors-only'
-      },
-      "reporters": ["mocha"],
-      "port": 9876,
-      "colors": true,
-      "logLevel": "error",
-      "autoWatch": true,
-      "singleRun": true,
-      "concurrency": "Infinity"
-    })
-  }
+      module: {
+        rules: [{
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader'
+        }]
+      }
+    },
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+    'reporters': ['mocha'],
+    'port': 9876,
+    'colors': true,
+    'logLevel': 'error',
+    'autoWatch': true,
+    'singleRun': true,
+    'concurrency': 'Infinity'
+  })
+}
