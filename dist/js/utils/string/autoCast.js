44c4407533ebdd6ac30d353b28f9cd8024eb86f7
Olivier Bossel <olivier.bossel@gmail.com>
2018-05-28 17:07:16 +0200

    starting update autoCast function

--- dist/js/utils/string/autoCast.js
+++ dist/js/utils/string/autoCast.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
@@ -8,21 +8,35 @@ exports.default = autoCast;
  * Auto cast the string into the correct variable type
  */
 function autoCast(string) {
+
+	try {
+		var obj = eval("(" + string + ")");
+		return obj;
+	} catch (e) {
+		// assume that the string passed is a string
+		return string;
+	}
+
 	// printed object
-	if (string === '[object Object]') return null;
-	// boolean values
-	if (string === 'false' || string === 'true' || string === 'undefined' || string === 'null' || !isNaN(string)) {
-		return eval(string);
-	}
-	// array
-	if (typeof string === 'string' && string.substr(0, 1) === '[') {
-		var val = eval(string);
-		if (val instanceof Array) return val;
-	}
-	// parse json
-	if (typeof string === 'string' && string.substr(0, 1) === '{') {
-		return eval('(' + string + ')');
-	}
-	// return the string if nothing can be casted
-	return string;
+	// if (string === '[object Object]') return null;
+	// // boolean values
+	// if (string === 'false'
+	// 	|| string === 'true'
+	// 	|| string === 'undefined'
+	// 	|| string === 'null'
+	// 	|| string === 'NaN'
+	// 	|| ! isNaN(string)) {
+	// 	return eval(string);
+	// }
+	// // array
+	// if (typeof(string) === 'string' && string.substr(0,1) === '[') {
+	// 	const val = eval(string);
+	// 	if (val instanceof Array) return val;
+	// }
+	// // parse json
+	// if (typeof(string) === 'string' && string.substr(0,1) === '{') {
+	// 	return eval('('+string+')');
+	// }
+	// // return the string if nothing can be casted
+	// return string;
 }
--- src/js/utils/string/autoCast.js
+++ src/js/utils/string/autoCast.js
@@ -2,25 +2,36 @@
  * Auto cast the string into the correct variable type
  */
 export default function autoCast(string) {
+
+	try {
+		const obj = eval(`(${string})`)
+		return obj
+	} catch(e) {
+		// assume that the string passed is a string
+		return string
+	}
+
+
 	// printed object
-	if (string === '[object Object]') return null;
-	// boolean values
-	if (string === 'false'
-		|| string === 'true'
-		|| string === 'undefined'
-		|| string === 'null'
-		|| ! isNaN(string)) {
-		return eval(string);
-	}
-	// array
-	if (typeof(string) === 'string' && string.substr(0,1) === '[') {
-		const val = eval(string);
-		if (val instanceof Array) return val;
-	}
-	// parse json
-	if (typeof(string) === 'string' && string.substr(0,1) === '{') {
-		return eval('('+string+')');
-	}
-	// return the string if nothing can be casted
-	return string;
+	// if (string === '[object Object]') return null;
+	// // boolean values
+	// if (string === 'false'
+	// 	|| string === 'true'
+	// 	|| string === 'undefined'
+	// 	|| string === 'null'
+	// 	|| string === 'NaN'
+	// 	|| ! isNaN(string)) {
+	// 	return eval(string);
+	// }
+	// // array
+	// if (typeof(string) === 'string' && string.substr(0,1) === '[') {
+	// 	const val = eval(string);
+	// 	if (val instanceof Array) return val;
+	// }
+	// // parse json
+	// if (typeof(string) === 'string' && string.substr(0,1) === '{') {
+	// 	return eval('('+string+')');
+	// }
+	// // return the string if nothing can be casted
+	// return string;
 }
