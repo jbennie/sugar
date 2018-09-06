7696b35c59efb4da5c1e22dc4297f5c390d4e4e5
Olivier Bossel <olivier.bossel@gmail.com>
2018-09-06 11:14:02 +0200

    add is/ddmmyyyyDate function

---
+++ doc/src/js/utils/is/opera.md
@@ -0,0 +1,14 @@
+# isOpera
+
+Detect if is opera
+
+### Example
+```js
+	import isOpera from 'coffeekraken-sugar/js/utils/is/opera'
+if (isOpera()) {
+  // do something cool
+}
+```
+Return **{ [Boolean](https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Boolean) }** true if is opera, false if not
+
+Author : Olivier Bossel [olivier.bossel@gmail.com](mailto:olivier.bossel@gmail.com) [https://olivierbossel.com](https://olivierbossel.com)
---
+++ js/utils/is/ddmmyyyyDate.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = isDdmmyyyyDate;
+/**
+ * Check if is a valid dd.mm.yyyy date
+ * @param    {String}    date    The date to check
+ * @return    {Boolean}    true if is valid, false if not
+ */
+function isDdmmyyyyDate(date) {
+  return (/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(date)
+  );
+}
