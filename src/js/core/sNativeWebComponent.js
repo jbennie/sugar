55f8b88789dc7d9b91072000c10a2c553212657d
Olivier Bossel <olivier.bossel@gmail.com>
2018-12-29 16:00:35 +0100

    change customElement polyfill from webcomponent to document-register-element

--- src/js/core/sNativeWebComponent.js
+++ src/js/core/sNativeWebComponent.js
@@ -1,12 +1,30 @@
-import '@webcomponents/webcomponentsjs'
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
+// import '@webcomponents/webcomponentsjs'
+// import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
+import 'document-register-element'
+import '@ungap/custom-elements-builtin'
 import { mix } from '../vendors/mixwith'
 import SWebComponentMixin from './SWebComponentMixin'
 export default function sNativeWebComponent(HTMLElementToExtend) {
+
+	HTMLElementToExtend = (function (OriginalHTMLElement) {
+		function BabelHTMLElement()
+		{
+			if (typeof Reflect == 'undefined' || typeof Reflect.construct != 'function' || typeof customElements == 'undefined') {
+				// Use your favorite polyfill.
+			}
+			const newTarget = this.__proto__.constructor;
+			return Reflect.construct(OriginalHTMLElement, [], newTarget);
+		}
+		Object.setPrototypeOf(BabelHTMLElement, OriginalHTMLElement);
+		Object.setPrototypeOf(BabelHTMLElement.prototype, OriginalHTMLElement.prototype);
+		return BabelHTMLElement;
+	})(HTMLElementToExtend);
+
 	if (typeof HTMLElementToExtend !== 'function'){
     	var _HTMLElementToExtend = function(){};
     	_HTMLElementToExtend.prototype = HTMLElementToExtend.prototype;
     	HTMLElementToExtend = _HTMLElementToExtend;
 	}
+
 	return mix(HTMLElementToExtend).with(SWebComponentMixin);
 }
