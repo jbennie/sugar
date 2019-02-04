05b9b9a00ce2dc260b1794b79583c79bb471a2d8
Olivier Bossel <olivier.bossel@gmail.com>
2019-02-04 14:49:46 +0100

    add sprintf function un strings utils

--- demo/src/js/app.js
+++ demo/src/js/app.js
@@ -1,35 +1,39 @@
-import 'babel-polyfill'
-import SWebComponent from '../../../js/core/SWebComponent'
-import toggleFullscreen from '../../../js/dom/toggleFullscreen'
-import detectInOutDirection from '../../../js/dom/detectInOutDirection'
-import addAnimationClass from '../../../js/dom/addAnimationClass'
-import imagesLoaded from '../../../js/dom/imagesLoaded'
-import appendStylesheetLink from '../../../js/dom/appendStylesheetLink'
-import linkLoaded from '../../../js/dom/linkLoaded'
-import querySelectorAllWithStyle from '../../../js/dom/querySelectorAllWithStyle'
-import backgroundImageLoaded from '../../../js/dom/backgroundImageLoaded'
-import unquote from '../../../js/utils/strings/unquote'
+import "babel-polyfill";
+import SWebComponent from "../../../js/core/SWebComponent";
+import toggleFullscreen from "../../../js/dom/toggleFullscreen";
+import detectInOutDirection from "../../../js/dom/detectInOutDirection";
+import addAnimationClass from "../../../js/dom/addAnimationClass";
+import imagesLoaded from "../../../js/dom/imagesLoaded";
+import appendStylesheetLink from "../../../js/dom/appendStylesheetLink";
+import linkLoaded from "../../../js/dom/linkLoaded";
+import querySelectorAllWithStyle from "../../../js/dom/querySelectorAllWithStyle";
+import backgroundImageLoaded from "../../../js/dom/backgroundImageLoaded";
+import unquote from "../../../js/utils/strings/unquote";
 
-import STimer from '../../../js/classes/STimer'
+import STimer from "../../../js/classes/STimer";
 
-import appendScriptTag from '../../../js/dom/appendScriptTag'
-import scriptLoaded from '../../../js/dom/scriptLoaded'
+import appendScriptTag from "../../../js/dom/appendScriptTag";
+import scriptLoaded from "../../../js/dom/scriptLoaded";
 
-import ltrim from '../../../js/utils/strings/ltrim'
-import rtrim from '../../../js/utils/strings/rtrim'
-import queryStringToObject from  '../../../js/utils/strings/queryStringToObject'
+import ltrim from "../../../js/utils/strings/ltrim";
+import rtrim from "../../../js/utils/strings/rtrim";
+import queryStringToObject from "../../../js/utils/strings/queryStringToObject";
 
-import keysFirst from '../../../js/utils/arrays/keysFirst'
-import keysLast from '../../../js/utils/arrays/keysLast'
+import keysFirst from "../../../js/utils/arrays/keysFirst";
+import keysLast from "../../../js/utils/arrays/keysLast";
 
-import isSamsumgBrowser from '../../../js/utils/is/samsungBrowser'
+import isSamsumgBrowser from "../../../js/utils/is/samsungBrowser";
 
-console.log('isSamsungBrowser', isSamsumgBrowser())
+import sprintf from "../../../js/utils/strings/sprintf";
+
+console.log("isSamsungBrowser", isSamsumgBrowser());
+
+console.log(sprintf("Hello %s", "World"));
+console.log(sprintf("Hello %(name)s", { name: "Dolly" }));
 
 // console.log('qs', queryStringToObject('?plop=hello&world=universe'))
 
-console.log(keysFirst(['a','b','d','g','c'], ['d','g','z','y']))
-
+console.log(keysFirst(["a", "b", "d", "g", "c"], ["d", "g", "z", "y"]));
 
 // const $link = appendStylesheetLink('http://coffeekraken.io/dist/css/style.css');
 // linkLoaded($link).then(() => {
@@ -51,63 +55,69 @@ console.log(keysFirst(['a','b','d','g','c'], ['d','g','z','y']))
 
 const timer = new STimer(1000, {
 	loop: true
-})
-let i = 0
+});
+let i = 0;
 timer.onTick(() => {
-	console.log('tick', i)
+	console.log("tick", i);
 	if (i >= 5) {
-		timer.pause()
+		timer.pause();
 	}
-	i++
-})
-timer.start()
+	i++;
+});
+timer.start();
 
 class MyComponentClass extends SWebComponent {
 	static get defaultProps() {
 		return {
-			hello: 'world'
-		}
+			hello: "world"
+		};
 	}
 	static get requiredProps() {
-		return ['hello']
+		return ["hello"];
 	}
 	static get physicalProps() {
-		return ['hello']
+		return ["hello"];
 	}
 	static defaultCss(componentName, componentNameDash) {
-		return `${componentNameDash} { content: 'hello world'; }`
+		return `${componentNameDash} { content: 'hello world'; }`;
 	}
 }
-const MyComponent = MyComponentClass.define('my-component');
+const MyComponent = MyComponentClass.define("my-component");
 
 setTimeout(() => {
-	document.body.appendChild(new MyComponent({
-		hello: 'universe'
-	}));
-	const elm = document.createElement('my-component');
+	document.body.appendChild(
+		new MyComponent({
+			hello: "universe"
+		})
+	);
+	const elm = document.createElement("my-component");
 	setTimeout(() => {
 		document.body.appendChild(elm);
 	}, 1000);
 }, 1000);
 
-const detectInOutDirectionElm = document.querySelector('#detectInOutDirection')
-detectInOutDirection(detectInOutDirectionElm, (direction, elm) => {
-	addAnimationClass(elm, `in-${direction}`)
-}, (direction, elm) => {
-	addAnimationClass(elm, `in-${direction}`)
-})
+const detectInOutDirectionElm = document.querySelector("#detectInOutDirection");
+detectInOutDirection(
+	detectInOutDirectionElm,
+	(direction, elm) => {
+		addAnimationClass(elm, `in-${direction}`);
+	},
+	(direction, elm) => {
+		addAnimationClass(elm, `in-${direction}`);
+	}
+);
 
-const fullscreenElm = document.querySelector('#fullscreen')
-fullscreenElm.addEventListener('click', (e) => {
-	toggleFullscreen(fullscreenElm)
-})
+const fullscreenElm = document.querySelector("#fullscreen");
+fullscreenElm.addEventListener("click", e => {
+	toggleFullscreen(fullscreenElm);
+});
 
-const $elms = querySelectorAllWithStyle('*', {
+const $elms = querySelectorAllWithStyle("*", {
 	backgroundImage: /^url/
-})
-$elms.forEach(($elm) => {
+});
+$elms.forEach($elm => {
 	backgroundImageLoaded($elm).then(() => {
-		console.log('loaded!!!')
-	})
-})
-console.log('querySelectorAllWithStyle', $elms)
+		console.log("loaded!!!");
+	});
+});
+console.log("querySelectorAllWithStyle", $elms);
