2396f8a964e72cbf2b59c8010a92c176d8c1e2ad
Olivier Bossel <olivier.bossel@gmail.com>
2019-02-10 20:53:23 +0100

    update inputAdditionalAttributes feature

--- js/features/inputAdditionalAttributes.js
+++ js/features/inputAdditionalAttributes.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-var _fastdom = require('fastdom');
+var _fastdom = require("fastdom");
 
 var _fastdom2 = _interopRequireDefault(_fastdom);
 
-var _querySelectorLive = require('../dom/querySelectorLive');
+var _querySelectorLive = require("../dom/querySelectorLive");
 
 var _querySelectorLive2 = _interopRequireDefault(_querySelectorLive);
 
@@ -24,28 +24,29 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function handleInputAttributes(eOrElm) {
 	var setDirty = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+	var forceDirty = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
 
 	var field = eOrElm.target ? eOrElm.target : eOrElm;
 	if (!field || !field.tagName) return;
 	switch (field.tagName) {
-		case 'INPUT':
-		case 'TEXTAREA':
-		case 'SELECT':
+		case "INPUT":
+		case "TEXTAREA":
+		case "SELECT":
 			_fastdom2.default.mutate(function () {
-				if (field.type && (field.type === 'checkbox' || field.type === 'radio')) return;
-				if (field.value && !field.hasAttribute('has-value')) {
-					field.setAttribute('has-value', true);
-					field.removeAttribute('empty');
-				} else if (field.value === undefined || field.value === null || field.value === '') {
-					field.removeAttribute('has-value');
-					field.removeAttribute('value');
-					if (!field.hasAttribute('empty')) {
-						field.setAttribute('empty', true);
+				if (field.type && (field.type === "checkbox" || field.type === "radio")) return;
+				if (field.value && !field.hasAttribute("has-value")) {
+					field.setAttribute("has-value", true);
+					field.removeAttribute("empty");
+				} else if (field.value === undefined || field.value === null || field.value === "") {
+					field.removeAttribute("has-value");
+					field.removeAttribute("value");
+					if (!field.hasAttribute("empty")) {
+						field.setAttribute("empty", true);
 					}
 				}
 				if (setDirty) {
-					if (!field.hasAttribute('dirty')) {
-						field.setAttribute('dirty', true);
+					if (!field.hasAttribute("dirty") && (field.value || forceDirty)) {
+						field.setAttribute("dirty", true);
 					}
 				}
 			});
@@ -57,12 +58,12 @@ function handleFormSubmitOrReset(e) {
 	// loop on each form elements
 	[].forEach.call(e.target.elements, function (field) {
 		// reset the field attributes
-		handleInputAttributes(field);
+		handleInputAttributes(field, true, true);
 		// stop here if is a submit
-		if (e.type === 'submit') return;
+		if (e.type === "submit") return;
 		// remove dirty attribute
 		_fastdom2.default.mutate(function () {
-			field.removeAttribute('dirty');
+			field.removeAttribute("dirty");
 		});
 	});
 }
@@ -71,7 +72,7 @@ function handleFormSubmitOrReset(e) {
 	handleInputAttributes(elm, false);
 });
 
-document.addEventListener('change', handleInputAttributes);
-document.addEventListener('keyup', handleInputAttributes);
-document.addEventListener('reset', handleFormSubmitOrReset);
-document.addEventListener('submit', handleFormSubmitOrReset);
+document.addEventListener("change", handleInputAttributes);
+document.addEventListener("keyup", handleInputAttributes);
+document.addEventListener("reset", handleFormSubmitOrReset);
+document.addEventListener("submit", handleFormSubmitOrReset);
--- src/js/features/inputAdditionalAttributes.js
+++ src/js/features/inputAdditionalAttributes.js
@@ -10,57 +10,68 @@
  * @author 		Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
  */
 
-import fastdom from 'fastdom'
-import __querySelectorLive from '../dom/querySelectorLive'
+import fastdom from "fastdom";
+import __querySelectorLive from "../dom/querySelectorLive";
 
-function handleInputAttributes(eOrElm, setDirty = true) {
+function handleInputAttributes(eOrElm, setDirty = true, forceDirty = false) {
 	const field = eOrElm.target ? eOrElm.target : eOrElm;
-	if ( ! field || ! field.tagName) return;
-	switch(field.tagName) {
-		case 'INPUT':
-		case 'TEXTAREA':
-		case 'SELECT':
+	if (!field || !field.tagName) return;
+	switch (field.tagName) {
+		case "INPUT":
+		case "TEXTAREA":
+		case "SELECT":
 			fastdom.mutate(() => {
-				if (field.type && (field.type === 'checkbox' || field.type === 'radio')) return;
-				if (field.value && ! field.hasAttribute('has-value')) {
-					field.setAttribute('has-value', true);
-					field.removeAttribute('empty');
-				} else if (field.value === undefined || field.value === null || field.value === '') {
-					field.removeAttribute('has-value');
-					field.removeAttribute('value');
-					if ( ! field.hasAttribute('empty')) {
-						field.setAttribute('empty', true);
+				if (
+					field.type &&
+					(field.type === "checkbox" || field.type === "radio")
+				)
+					return;
+				if (field.value && !field.hasAttribute("has-value")) {
+					field.setAttribute("has-value", true);
+					field.removeAttribute("empty");
+				} else if (
+					field.value === undefined ||
+					field.value === null ||
+					field.value === ""
+				) {
+					field.removeAttribute("has-value");
+					field.removeAttribute("value");
+					if (!field.hasAttribute("empty")) {
+						field.setAttribute("empty", true);
 					}
 				}
 				if (setDirty) {
-					if ( ! field.hasAttribute('dirty')) {
-						field.setAttribute('dirty', true);
+					if (
+						!field.hasAttribute("dirty") &&
+						(field.value || forceDirty)
+					) {
+						field.setAttribute("dirty", true);
 					}
 				}
 			});
-		break;
+			break;
 	}
 }
 
 function handleFormSubmitOrReset(e) {
 	// loop on each form elements
-	[].forEach.call(e.target.elements, (field) => {
+	[].forEach.call(e.target.elements, field => {
 		// reset the field attributes
-		handleInputAttributes(field);
+		handleInputAttributes(field, true, true);
 		// stop here if is a submit
-		if (e.type === 'submit') return;
+		if (e.type === "submit") return;
 		// remove dirty attribute
 		fastdom.mutate(() => {
-			field.removeAttribute('dirty');
+			field.removeAttribute("dirty");
 		});
 	});
 }
 
-__querySelectorLive('select, textarea, input:not([type="submit"])', (elm) => {
+__querySelectorLive('select, textarea, input:not([type="submit"])', elm => {
 	handleInputAttributes(elm, false);
 });
 
-document.addEventListener('change', handleInputAttributes);
-document.addEventListener('keyup', handleInputAttributes);
-document.addEventListener('reset', handleFormSubmitOrReset);
-document.addEventListener('submit', handleFormSubmitOrReset);
+document.addEventListener("change", handleInputAttributes);
+document.addEventListener("keyup", handleInputAttributes);
+document.addEventListener("reset", handleFormSubmitOrReset);
+document.addEventListener("submit", handleFormSubmitOrReset);
