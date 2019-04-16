05990df17a425e304a7d717c6e0f3729448c307f
Olivier Bossel <olivier.bossel@gmail.com>
2019-04-16 14:28:56 +0000

    update webcomponent compatibility layer

--- src/js/core/sNativeWebComponent.js
+++ src/js/core/sNativeWebComponent.js
@@ -1,25 +1,18 @@
 import "document-register-element";
 import "@ungap/custom-elements-builtin";
-import isSafari from "../utils/is/safari";
+// import isSafari from "../utils/is/safari";
 import { mix } from "../vendors/mixwith";
 import SWebComponentMixin from "./SWebComponentMixin";
-import isSamsumgBrowser from "../utils/is/samsungBrowser";
-import isUcBrowser from "../utils/is/ucBrowser";
+// import isSamsumgBrowser from "../utils/is/samsungBrowser";
+// import isUcBrowser from "../utils/is/ucBrowser";
 const extendsStack = {};
 export default function sNativeWebComponent(HTMLElementToExtend) {
-	if (!isSafari() && !isSamsumgBrowser() && !isUcBrowser()) {
+	// if (!isSafari() && !isSamsumgBrowser() && !isUcBrowser()) {
 		HTMLElementToExtend = (function(OriginalHTMLElement) {
 			if (!window[OriginalHTMLElement.name]) return OriginalHTMLElement;
 			if (extendsStack[OriginalHTMLElement.name])
 				return extendsStack[OriginalHTMLElement.name];
 			function BabelHTMLElement() {
-				if (
-					typeof Reflect == "undefined" ||
-					typeof Reflect.construct != "function" ||
-					typeof customElements == "undefined"
-				) {
-					// Use your favorite polyfill.
-				}
 				const newTarget = this.__proto__.constructor;
 				return Reflect.construct(OriginalHTMLElement, [], newTarget);
 			}
@@ -31,13 +24,8 @@ export default function sNativeWebComponent(HTMLElementToExtend) {
 			extendsStack[HTMLElementToExtend.name] = BabelHTMLElement;
 			return BabelHTMLElement;
 		})(HTMLElementToExtend);
-	}
-
-	// if (typeof HTMLElementToExtend !== "function") {
-	// 	var _HTMLElementToExtend = function() {};
-	// 	_HTMLElementToExtend.prototype = HTMLElementToExtend.prototype;
-	// 	HTMLElementToExtend = _HTMLElementToExtend;
 	// }
+
 
 	return mix(HTMLElementToExtend).with(SWebComponentMixin);
 }
